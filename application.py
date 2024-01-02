77523f30499524645f5e0f21fa9382135d456a12
TarangKh <tarangkharkar4448@gmail.com>
2024-01-02 11:40:39 +0530

    second commit, added exception handling

--- application.py
+++ application.py
@@ -25,30 +25,38 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    brand = request.form.get("brand")
-    model_name = request.form.get("model")
-    processor_brand = request.form.get("processor_brand")
-    processor_name = request.form.get("processor_name")
-    gen = int(request.form.get("gen"))
-    ram = int(request.form.get("ram"))
-    ram_type  = request.form.get("ram_type")
-    ssd = int(request.form.get("ssd"))
-    hdd = int(request.form.get("hdd"))
-    os = request.form.get("os")
-    os_bit = int(request.form.get("os_bit"))
-    gpu = int(request.form.get("gpu"))
-    Touchscreen = int(request.form.get("Touchscreen"))
-    model = pickle.load(open("model.pkl", "rb"))
-    prediction = model.predict(pd.DataFrame([[brand, model_name, processor_brand, processor_name, gen, ram, ram_type, ssd, hdd, os, os_bit, gpu, Touchscreen]], columns=['brand', 'model', 'processor_brand', 'processor_name',
-       'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'hdd', 'os', 'os_bit',
-       'graphic_card_gb', 'Touchscreen']))
-    # prediction = model.predict(pd.DataFrame([["Lenovo", "A6-9225", "AMD", "A6-9225 Processor", 10, 4, "DDR4", 256, 1024, "Windows", 64, 2, 1]],
-    #                           columns=['brand', 'model', 'processor_brand', 'processor_name',
-    #    'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'hdd', 'os', 'os_bit',
-    #    'graphic_card_gb', 'Touchscreen']))
-    # print([brand, model_name, processor_brand, processor_name, gen, ram, ram_type, ssd, hdd, os, os_bit, gpu, Touchscreen])
+    try:
+        brand = request.form.get("brand")
+        model_name = request.form.get("model")
+        processor_brand = request.form.get("processor_brand")
+        processor_name = request.form.get("processor_name")
+        gen = int(request.form.get("gen"))
+        ram = int(request.form.get("ram"))
+        ram_type  = request.form.get("ram_type")
+        ssd = int(request.form.get("ssd"))
+        hdd = int(request.form.get("hdd"))
+        os = request.form.get("os")
+        os_bit = int(request.form.get("os_bit"))
+        gpu = int(request.form.get("gpu"))
+        Touchscreen = int(request.form.get("Touchscreen"))
+        model = pickle.load(open("model.pkl", "rb"))
+        prediction = model.predict(pd.DataFrame([[brand, model_name, processor_brand, processor_name, gen, ram, ram_type, ssd, hdd, os, os_bit, gpu, Touchscreen]], columns=['brand', 'model', 'processor_brand', 'processor_name',
+        'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'hdd', 'os', 'os_bit',
+        'graphic_card_gb', 'Touchscreen']))
+        # prediction = model.predict(pd.DataFrame([["Lenovo", "A6-9225", "AMD", "A6-9225 Processor", 10, 4, "DDR4", 256, 1024, "Windows", 64, 2, 1]],
+        #                           columns=['brand', 'model', 'processor_brand', 'processor_name',
+        #    'processor_gnrtn', 'ram_gb', 'ram_type', 'ssd', 'hdd', 'os', 'os_bit',
+        #    'graphic_card_gb', 'Touchscreen']))
+        # print([brand, model_name, processor_brand, processor_name, gen, ram, ram_type, ssd, hdd, os, os_bit, gpu, Touchscreen])
+        
+        return str(int(prediction[0]))
     
-    return str(int(prediction[0]));
+    except (ValueError):
+        return "ValueError"
+    except Exception as e:
+        return render_template("exception.html", e=e)
+
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port="5000")
